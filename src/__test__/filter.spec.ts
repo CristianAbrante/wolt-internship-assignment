cf89b0a155a8e371f4ab74951e6c1cda16eed4b0
Cristian Abrante <cristian@abrante.me>
2021-01-30 19:15:16 +0000

    Location filter tests added

--- src/__test__/filter.spec.ts
+++ src/__test__/filter.spec.ts
@@ -53,3 +53,47 @@ describe("LOCATION - Filter test", () => {
     expect(restaurants.map((r) => r.name)).toMatchObject(closeRestaurantsMock3);
   });
 });
+
+// As popularity have the same filtering criteria as location
+// we can reuse the mock data.
+describe("POPULARITY - Filter test", () => {
+  test("Filter empty array", () => {
+    const restaurants = filterRestaurants(
+      [],
+      referenceLocationMock1,
+      "popularity"
+    );
+
+    expect(restaurants).toMatchObject([]);
+  });
+
+  test("Mock restaurants 1", () => {
+    const restaurants = filterRestaurants(
+      restaurantsMock1,
+      referenceLocationMock1,
+      "popularity"
+    );
+
+    expect(restaurants).toMatchObject(closeRestaurantsMock1);
+  });
+
+  test("Mock restaurants 2", () => {
+    const restaurants = filterRestaurants(
+      restaurantsMock2,
+      referenceLocationMock2,
+      "popularity"
+    );
+
+    expect(restaurants).toMatchObject(closeRestaurantsMock2);
+  });
+
+  test("Mock restaurants 3", () => {
+    const restaurants = filterRestaurants(
+      restaurantsMock3,
+      referenceLocationMock3,
+      "popularity"
+    );
+
+    expect(restaurants.map((r) => r.name)).toMatchObject(closeRestaurantsMock3);
+  });
+});
